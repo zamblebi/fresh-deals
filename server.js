5d0e931fa13d926df74b9a0c31f138775de9f1da
Samuel Bouye <bouyesamuel@gmail.com>
2020-10-13 01:00:21 +0000

    add mongoose mongodb DRM and initalisate

--- server.js
+++ server.js
@@ -3,12 +3,21 @@ const app = express();
 const route = require('./routes/route')(app);
 const port = 3000
 
+const mongoose = require('mongoose')
+
 const MongoClient = require('mongodb').MongoClient;
-const DatabaseConnection = require('./models/connection')(MongoClient)
+// const DatabaseConnection = require('./models/connection')(MongoClient)
+
+mongoose.connect('mongodb://localhost/Okey', {useNewUrlParser: true})
 
 
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', function(){
+    console.log('Db connected')
+})
 
-DatabaseConnection;
+// DatabaseConnection;
 route;
 
 
