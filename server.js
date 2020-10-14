4d1f95e5b4b259dc3b2aa416fe92cfe122e06b6e
Samuel Bouye <bouyesamuel@gmail.com>
2020-10-14 14:52:23 +0000

    show sample article from mongoose

--- server.js
+++ server.js
@@ -3,21 +3,12 @@ const app = express();
 const route = require('./routes/route')(app);
 const port = 3000
 
-const mongoose = require('mongoose')
-
-const MongoClient = require('mongodb').MongoClient;
-// const DatabaseConnection = require('./models/connection')(MongoClient)
-
-mongoose.connect('mongodb://localhost/Okey', {useNewUrlParser: true})
-
-
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'))
-db.once('open', function(){
-    console.log('Db connected')
-})
+// Database Connection 
+const database_connect = require('./models/connection');
+const list = require('./models/listArticles')
 
 // DatabaseConnection;
+// listArticles;
 route;
 
 
