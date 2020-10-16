5c63a15ed0115c25efe107b11ffb73011b3845e4
Samuel Bouye <bouyesamuel@gmail.com>
2020-10-16 10:29:35 +0000

    intro ajustement

--- client/src/components/Intro.jsx
+++ client/src/components/Intro.jsx
@@ -1,24 +1,27 @@
 import React, { useState } from 'react'
-
+import './Intro.css'
 const Intro = () => {
     return (
-        <div className="Intro">
+        <div className="intro">
             <div>
                 <h2>Site d'annonce gratuit dans le monde</h2>
                 <h4>Choisir une ville</h4>
-                <ul>
-                    <li>Abidjan</li>
-                    <li>Daloa</li>
-                    <li>Zuenoula</li>
-
-                    <li>Man</li>
-                    <li>Bingerville</li>
-                    <li>Seguela</li>
+                <ul className="intro_town">
+                    <div>
+                        <li>Abidjan</li>
+                        <li>Daloa</li>
+                        <li>Zuenoula</li>
+                    </div>
+                    <div>
+                        <li>Man</li>
+                        <li>Bingerville</li>
+                        <li>Seguela</li>
+                    </div>
                 </ul>
                 <button>Publiez votre annonce gratuitement</button>
             </div>
             <div>
-                <ul>
+                <ul className="offer_presentation">
                     <li>Vendez tout GRATUITEMENT !</li>
                     <li>Des centaines d'acheteur chaque jour</li>
                     <li>Ajoutez des images pour vendre plus vite !</li>
