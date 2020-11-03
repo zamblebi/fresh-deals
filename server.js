2cfa01462e9e04ddb5a72c3c7bf783965b75bf65
Samuel Bouye <bouyesamuel@gmail.com>
2020-11-03 18:09:23 +0000

    refactoring all code backend and add mongdb cloud cluster

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
-const express = require('express');
+import  express from 'express';
+import  mongoose from 'mongoose'
+
+import apiV2 from './routes/route.js';
+
 const app = express();
-const route = require('./routes/route')(app);
-const port = 3000
-
-// Database Connection 
-const database_connect = require('./models/connection');
-const list = require('./models/listArticles')
-
-// DatabaseConnection;
-// listArticles;
-route;
+const port = 4005
 
 
+//Middleware
+
+    // Route endpoint middleware router
+app.use('/api',apiV2);
+
+
+// Database Config
+const urlMongoDB = "mongodb+srv://admin:W1CDPhwJHxZ6FaXR@cluster0test.fzma4.mongodb.net/freshdeals_db?retryWrites=true&w=majority"
+mongoose.connect(urlMongoDB, {
+    useNewUrlParser: true,
+    useUnifiedTopology:true
+})
+
+
+// Listening;
 app.listen(port, ()=>{
     console.log('App running on port 4005')
 })
