3f8bb4f0c2f88b90b715bb4924d5d3352fa59c2b
Samuel Bouye <bouyesamuel@gmail.com>
2020-11-03 14:23:27 +0000

    separate frontend

--- client/src/components/Carousel/DataSlider.js
+++
@@ -1,5 +0,0 @@
-export default [
-    {image: '', title: "Fist Slide simple text" },
-    {image: '', title: "Second Hello world too" },
-    {image: '', title: "Thertee Slide Simple again text" },
-]
